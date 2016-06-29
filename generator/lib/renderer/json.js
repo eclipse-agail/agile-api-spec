417bcd94debb5ca24b8d1948a61b83c2d9c5596d
Luca Capra <luca.capra@gmail.com>
2016-06-29 08:48:14 +0200

    moved spec generator to an autonomous repository

--- generator/lib/renderer/json.js
+++
@@ -1,8 +0,0 @@
-
-var _ = require("lodash");
-
-module.exports = function(definition) {
-  return new Promise(function(resolve, reject) {
-    resolve(JSON.stringify(definition.toJSON(), null, 2));
-  });
-};
